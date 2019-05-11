9faeff6d0c2c6f791c6d38632dc0ac39eac5540f
Jose Lopez <joseafdl@yahoo.com>
2019-05-11 20:09:34 +0200

    TicTacToe ready self-training, self-player and arena-player

---
+++ examples/rts/TrumpDefense2020-master/TD2020/Source/TD2020/TD2020.cpp
@@ -0,0 +1,8 @@
+// No copyright - copy as you please
+
+#include "TD2020.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TD2020, "TD2020" );
+
+DEFINE_LOG_CATEGORY(LogTD2020);
